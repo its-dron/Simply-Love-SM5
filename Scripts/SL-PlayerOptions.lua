5547f79be0b7b27802656cb54116a29f6c7894d2
its-dron <dron@alum.mit.edu>
2025-07-28 10:30:31 -0400

    Empty Break -> Rest Break

--- Scripts/SL-PlayerOptions.lua
+++ Scripts/SL-PlayerOptions.lua
@@ -695,7 +695,7 @@ local Overrides = {
 	-------------------------------------------------------------------------
 	MeasureCounterOptions = {
 		SelectType = "SelectMultiple",
-		Values = { "MeasureCounterLeft", "MeasureCounterUp", "MeasureCounterVert", "BrokenRun", "RunTimer", "HighlightEmptyBreaks", "ShowEmptyBreakCountdown"},
+		Values = { "MeasureCounterLeft", "MeasureCounterUp", "MeasureCounterVert", "BrokenRun", "RunTimer", "HighlightRestBreaks", "ShowRestBreakCountdown"},
 	},
 	-------------------------------------------------------------------------
 	LifeBarOptions = {
@@ -769,12 +769,12 @@ local Overrides = {
 		SelectType = "SelectMultiple",
 		Values = { "HideEarlyDecentWayOffJudgments", "HideEarlyDecentWayOffFlash" }
 	},
-	EmptyBreakThreshold = {
+	RestBreakThreshold = {
 		Choices = { "1", "2", "4", "8", "12", "16" },
 		Values = { 1, 2, 4, 8, 12, 16 },
 		LoadSelections = function(self, list, pn)
 			local mods = SL[ToEnumShortString(pn)].ActiveModifiers
-			local threshold = mods.EmptyBreakThreshold or 1
+			local threshold = mods.RestBreakThreshold or 1
 			for i, val in ipairs(self.Values) do
 				if val == threshold then
 					list[i] = true
@@ -787,7 +787,7 @@ local Overrides = {
 			local mods = SL[ToEnumShortString(pn)].ActiveModifiers
 			for i, val in ipairs(self.Values) do
 				if list[i] then
-					mods.EmptyBreakThreshold = val
+					mods.RestBreakThreshold = val
 					break
 				end
 			end
